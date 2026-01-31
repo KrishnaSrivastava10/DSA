df2ba4099310208c45db9b69cf973029f6eba556
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:52:41 +0530

    Update pattern15.java

--- Patterns/pattern15.java
+++ Patterns/pattern15.java
@@ -1,1 +1,8 @@
-
+class pattern15{
+        for(int i=n-1;i>=0;i--){
+            for(char ch='A';ch<='A'+i;ch++){
+                System.out.print(ch);
+        }
+        System.out.println();
+        }
+}
