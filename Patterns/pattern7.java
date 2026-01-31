046b7ac027a200910a3ef3538cdf4ccdf5aa27b4
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:47:55 +0530

    Update pattern7.java

--- Patterns/pattern7.java
+++ Patterns/pattern7.java
@@ -1,1 +1,12 @@
-
+class pattern7{
+       for(int i=0;i<n;i++){
+            int space=n/2+1;
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
