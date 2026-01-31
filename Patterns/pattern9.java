5fe328b5b45e7d18b33b1bd599061a56400e3bec
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:48:57 +0530

    Update pattern9.java

--- Patterns/pattern9.java
+++ Patterns/pattern9.java
@@ -1,1 +1,22 @@
-
+class pattern9{
+        for(int i=0;i<10/2;i++){
+         
+            for(int j=0;j<10/2-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<10/2;i++){
+            
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*5-(2*i+1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
