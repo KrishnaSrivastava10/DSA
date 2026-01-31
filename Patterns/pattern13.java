18f14e270a39f97e447b704e7c2631f60d8cdc76
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:51:50 +0530

    Update pattern13.java

--- Patterns/pattern13.java
+++ Patterns/pattern13.java
@@ -1,1 +1,11 @@
-
+class pattern13{
+        int count = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count + " ");
+                count++;
+            }
+            
+        System.out.println();
+        }
+}
