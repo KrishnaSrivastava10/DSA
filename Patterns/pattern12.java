3c6a2e14fc673f567e8ae08d41b14a9e08710d3e
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:51:01 +0530

    Update pattern12.java

--- Patterns/pattern12.java
+++ Patterns/pattern12.java
@@ -1,1 +1,16 @@
-
+class pattern11{
+        int spaces = 2*(n-1);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j + " ");
+            }
+            for(int j=1;j<=spaces;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j + " ");
+            }
+            spaces -= 2;
+        System.out.println();
+        }
+}
