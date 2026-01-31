b99ceff086faf762aed07da66625e3834eef0017
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:49:30 +0530

    Update pattern10.java

--- Patterns/pattern10.java
+++ Patterns/pattern10.java
@@ -1,1 +1,16 @@
-
+class pattern9{
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        int st = n-1;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<st;j++){
+                System.out.print("* ");
+            }
+            st--;
+            System.out.println();
+        }
+}
