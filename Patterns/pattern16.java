9ef4674ae383ff96613d728a4aea3c9f4e34043d
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:53:05 +0530

    Update pattern16.java

--- Patterns/pattern16.java
+++ Patterns/pattern16.java
@@ -1,1 +1,9 @@
-
+class pattern16{
+        for(int i=0;i<n;i++){
+            char ch = (char)('A'+i);
+            for(int j=0;j<=i;j++){
+                System.out.print(ch);
+        }
+        System.out.println();
+        }
+}
