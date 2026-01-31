17506572f2ef775b37f04eac6fd9571a73e3ec4d
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:45:19 +0530

    Update pattern3.java

--- Patterns/pattern3.java
+++ Patterns/pattern3.java
@@ -1,1 +1,8 @@
-
+class pattern3{
+     for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(j+1 + " ");
+            }
+            System.out.println("");
+      }
+}
