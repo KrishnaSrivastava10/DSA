2133a62c8d803c82d263463f6a9a87e05dfcc3f2
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:52:18 +0530

    Update pattern14.java

--- Patterns/pattern14.java
+++ Patterns/pattern14.java
@@ -1,1 +1,8 @@
-
+class pattern14{
+        for(int i=0;i<n;i++){
+            for(char ch='A';ch<='A'+i;ch++){
+                System.out.print(ch);
+        }
+        System.out.println();
+        }
+}
