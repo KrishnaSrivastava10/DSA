32eb8ae9dc1cda5befc232b2b32ee4ff842406b9
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:53:28 +0530

    Update pattern17.java

--- Patterns/pattern17.java
+++ Patterns/pattern17.java
@@ -1,1 +1,18 @@
-
+class pattern17{
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            char ch = 'A';
+            int breakthrough = (2*i+1) / 2;
+            for(int j=1;j<=(2*i)+1;j++){
+                System.out.print(ch);
+                if(j <= breakthrough){
+                    ch++;
+                }else{
+                    ch--;
+                } 
+            }
+        System.out.println();
+        }
+}
