682150f06e025f5f0ee4f3b14b2920b3d94446af
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:46:01 +0530

    Update pattern4.java

--- Patterns/pattern4.java
+++ Patterns/pattern4.java
@@ -1,1 +1,8 @@
-
+class pattern4{
+     for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1 + " ");
+            }
+            System.out.println("");
+      }
+}
