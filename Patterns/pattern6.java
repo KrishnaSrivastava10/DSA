c57e07e8b10129d3223a28b9ac9236249998f2a5
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:47:11 +0530

    Update pattern6.java

--- Patterns/pattern6.java
+++ Patterns/pattern6.java
@@ -1,1 +1,8 @@
-
+class pattern6{
+       for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(j+1 + " ");
+            }
+            System.out.println("");
+      }
+}
