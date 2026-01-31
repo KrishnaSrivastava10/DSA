5b646e5a13d34528957961554b7a0db0ef2d32d9
Krishna Srivastava <krishna.techspace@gmail.com>
2026-01-31 09:44:18 +0530

    Update pattern2.java

--- Patterns/pattern2.java
+++ Patterns/pattern2.java
@@ -1,1 +1,9 @@
 
+class pattern2{
+     for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+     }
+}
