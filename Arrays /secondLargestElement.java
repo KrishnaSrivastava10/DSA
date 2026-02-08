463f26f94993c69c57f30e397198c3ae55629098
Krishna Srivastava <krishna.techspace@gmail.com>
2026-02-08 15:40:43 +0530

    Update secondLargestElement.java

--- Arrays /secondLargestElement.java
+++ Arrays /secondLargestElement.java
@@ -1,1 +1,15 @@
+// BRUTE FORCE 
 
+// firstly sort the array and then call this - 
+
+for(int i=n-2;i>=0;i--){
+  if(arr[i] != largest){
+    secondLargest = arr[i];
+    break;
+  }
+}
+
+// TC -
+// SORT - O(nlogn) 
+// Traversing to find second largest - O(n)
+// = O(nlogn) 
