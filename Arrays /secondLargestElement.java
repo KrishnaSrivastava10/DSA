fff356dbdf4f4584e50d5d93d16e71d2fa3dc3a6
Krishna Srivastava <krishna.techspace@gmail.com>
2026-02-08 15:49:56 +0530

    Update secondLargestElement.java

--- Arrays /secondLargestElement.java
+++ Arrays /secondLargestElement.java
@@ -13,3 +13,32 @@ for(int i=n-2;i>=0;i--){
 // SORT - O(nlogn) 
 // Traversing to find second largest - O(n)
 // = O(nlogn) 
+
+
+
+// BETTER APPROACH
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        int largest = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest) largest = arr[i];
+        }
+        
+        int secondLargest = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>secondLargest && arr[i]!=largest) secondLargest = arr[i];
+        }
+        
+        return secondLargest;
+    }
+}
+
+// TC - 
+// O(2N)
+
+
+
+// OPTIMAL APPROACH
+
