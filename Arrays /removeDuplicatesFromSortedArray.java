87601fa619f8a2328d188984a83a550f1c16bb6a
Krishna Srivastava <krishna.techspace@gmail.com>
2026-02-14 14:42:22 +0530

    Update brute removeDuplicatesFromSortedArray.java

--- Arrays /removeDuplicatesFromSortedArray.java
+++ Arrays /removeDuplicatesFromSortedArray.java
@@ -1,1 +1,23 @@
+//BRUTE APPROACH
+class Solution {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        Set<Integer> set= new LinkedHashSet<>();
+        for(int x: arr){    // - nlogn
+            set.add(x);
+        }
+        
+        int index = 0;
+        int n = set.size();
+        int[] result = new int[n];
+        
+        for(int x:set){    // - n
+            result[index++] = x;
+        }
+        
+        return new ArrayList<>(set);
+        
+    }
+}
 
+// TC -
+// O(nlogn)
